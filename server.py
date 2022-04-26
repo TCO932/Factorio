b1ef477dd25b9f5bdf3d485b84e487db29f024a6
vasya pupkin <64690879+TCO932@users.noreply.github.com>
2022-04-26 16:18:26 +0000

    added detailedCraft route

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template_string
 from threading import Thread
-from factorio import getResources, getFactoryCount, getRecipes
+from factorio import getResources, getFactoryCount, getRecipes, getDetailedcraft
 # http://10.1.1.1:5000/login?username=alex&password=pw1
 
 app = Flask('')
@@ -24,6 +24,17 @@ def getFac():
     item_name = request.args.get('item')
     if (item_name is None): return "Item Name is undefined"
     return getFactoryCount(item_name)
+
+@app.route('/detailedCraft')
+def detailedCraft():
+    item_name = request.args.get('item')
+    speed = request.args.get('speed')
+    assembling_machine = request.args.get('assembling_machine')
+    productivity = request.args.get('productivity')
+    
+    if (item_name is None): return "Item Name is undefined"
+        
+    return getDetailedcraft(item_name, speed, assembling_machine, productivity)
 
 def run():
   app.run(host='0.0.0.0',port=8080)
